d04e494f2e34517c4086c04c4f7f2f3b3f29b877
karauli01king <130498383+KarauliKing@users.noreply.github.com>
2026-01-15 20:43:43 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "YOUR_TELEGRAM_BOT_TOKEN",
-  id:"Enter Your Id",
+  token: "8216966391:AAGRm67KHuex0d-o_qNW-juGPN03iGmaWRc",
+  id:"8322615047",
   address:"Enter your url"
 };
